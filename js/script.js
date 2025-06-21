c13462cdc43f357851375c16a8f1e2429865ebe5
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-06-22 01:32:58 +0600

    popular section complete

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ---------------- slider plugin 
+
+// $('.popularSlider').slick({
+//   infinite: true,
+//   slidesToShow: 4,
+//   slidesToScroll: 3,
+//   prevArrow: '<i class="fa-regular fa-circle-left"></i>',
+//   nextArrow: '<i class="fa-regular fa-circle-right"></i>',
+
+// });
+
+
+$('.popularSlider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  prevArrow: '<i class="fa-regular both nr fa-circle-right"></i>',
+  nextArrow: '<i class="fa-regular both pr fa-circle-left"></i>',
+});
