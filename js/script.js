ef20627ba38574fcbefc10b3078fc78379ee647c
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-06-22 12:18:27 +0600

    recommended section complete

--- js/script.js
+++ js/script.js
@@ -8,3 +8,11 @@ $('.popularSlider').slick({
   prevArrow: '<i class="fa-regular both nr fa-circle-right"></i>',
   nextArrow: '<i class="fa-regular both pr fa-circle-left"></i>',
 });
+
+
+// ----------------counter plugin 
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
